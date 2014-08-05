5f40ab74d9f30e26eacf6b867178dd693e47462a
nileaves <1059994896@qq.com>
2014-08-05 21:18:46 +0800

    分页定位  导航条  找优惠商家   商品（没完成）

--- js/pagination.js
+++ js/pagination.js
@@ -16,7 +16,7 @@ function pagination(){
 	//设置分页效果
     var str = "<li><a href='#' onclick='laquo()'>&laquo;</a></li>"
     for(i=0;i<pageSize;i++){
-        str += "<li><a href='#' onclick='page_jump("+(i+1)+")'>"+(i+1)+"</a></li>";
+        str += "<li><a href='#' onclick='goods_show("+(i+1)+")'>"+(i+1)+"</a></li>";
     }
     str += "<li><a href='#' onclick='raquo()'>&raquo;</a></li>";
     $(".pagination").html(str);
@@ -35,12 +35,10 @@ function goods_show(page){
     if(page<pageSize){
         for(i=0;i<pageNum;i++){
         	str +="<div class='row'><div class='col-xs-5'><div class='thumbnail'><img src='"+data[i].pic_src+"'></div></div><div class='col-xs-7'><h5>"+data[i].name+"</h5><p>"+data[i].num+"</p></div></div></div>";
-            //str += "<div class='col-md-3 nb'><div class='row'><div class='col-md-6 newbook'><img src='../../admin/"+bookData[(page-1)*pageNum+i]["book_picture"]+"'></div><div class='col-md-6'><p><a href='bookInfo.php?id="+bookData[(page-1)*pageNum+i]["book_id"]+"'>"+bookData[(page-1)*pageNum+i]["book_name"]+"</a></p><p>作者："+bookData[(page-1)*pageNum+i]["book_author"]+"</p><p>售价："+bookData[(page-1)*pageNum+i]["book_price"]+"元</p><p>数量："+bookData[(page-1)*pageNum+i]["book_num"]+"</p></div></div></div>";
         }
     }else{
        for(i=0;i<num-pageNum*(page-1);i++){
        	str +="<div class='row'><div class='col-xs-5'><div class='thumbnail'><img src='"+data[i].pic_src+"'></div></div><div class='col-xs-7'><h5>"+data[i].name+"</h5><p>"+data[i].num+"</p></div></div></div>";
-            //str += "<div class='col-md-3 nb'><div class='row'><div class='col-md-6 newbook'><img src='../../admin/"+bookData[(page-1)*pageNum+i]["book_picture"]+"'></div><div class='col-md-6'><p><a href='bookInfo.php?id="+bookData[(page-1)*pageNum+i]["book_id"]+"'>"+bookData[(page-1)*pageNum+i]["book_name"]+"</a></p><p>作者："+bookData[(page-1)*pageNum+i]["book_author"]+"</p><p>售价："+bookData[(page-1)*pageNum+i]["book_price"]+"元</p><p>数量："+bookData[(page-1)*pageNum+i]["book_num"]+"</p></div></div></div>";
         } 
     }
     $(".goods").html(str);
