989707bc162e1744a2e87d16e69261d0a9cb4a63
nileaves <1059994896@qq.com>
2014-08-11 20:35:03 +0800

    修改分页
    样式还是有点问题

--- js/pagination.js
+++ js/pagination.js
@@ -14,12 +14,12 @@ function pagination(){
 	pageSize = num>pageNum?Math.ceil(num/pageNum):1;	//确定页数
     
 	//设置分页效果
-    var str = "<a href='#scrollspy_goods' onclick='laquo()'><button class='btn btn-default'>上一页</button></a><select class='form-control'>";
+    var str = "<div class='col-xs-3'><a href='#scrollspy_goods' onclick='laquo()'><button class='btn btn-default btn-sm'>上一页</button></a></div><div class='col-xs-5'><select class='form-control input-sm'>";
     for(i=0;i<pageSize;i++){
         str += "<option value='"+(i+1)+"'>第 "+(i+1)+"/"+pageSize+" 页</option>";
     }
    
-    str += "</select><a href='#scrollspy_goods' onclick='raquo()'><button class='btn btn-default'>下一页</button></a>";
+    str += "</select></div><div class='col-xs-3'><a href='#scrollspy_goods' onclick='raquo()'><button class='btn btn-default btn-sm'>下一页</button></a>";
     $(".pagination").html(str);
     //默认显示第一页
     goods_show(currentPage);
