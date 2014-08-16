c3f11d712851d05bf71485b0df0b2c7495992d39
nileaves <1059994896@qq.com>
2014-08-16 08:45:00 +0800

    联级下拉列表

--- js/linkage_menu.js
+++ js/linkage_menu.js
@@ -1,24 +1,26 @@
 //大类
 var str_l = "";
-for(i=0;i<class_large.length;i++){
-	str_l += "<option>"+class_large[i].large_name+"</option>";
+for(i=0;i<class_data.length;i++){
+	str_l += "<option>"+class_data[i].large_name+"</option>";
 }
 $(".class_large").append(str_l);
-//小类
+//小类(默认)
 var str_s = "";
-for(i=0;i<class_data.class_large_1.length;i++){
-	str_s += "<option>"+class_data.class_large_1[i].class_small+"</option>";
-	//alert(class_data.class_large_1[i].class_small);
+for(i=0;i<class_large_1.length;i++){
+	str_s += "<option>"+class_large_1[i].small_name+"</option>";
 }
 $(".class_small").append(str_s);
 $(".class_large").change(function(){
 	var selected_val = $(".class_large").val();
 	var str_change = "";
-	var i = 1;
-	alert(eval(class_data[0].class_small_i));
-	alert("dd");
+	for(i=0;i<eval(selected_val).length;i++){
+		str_change += "<option>"+eval(selected_val)[i].small_name+"</option>";
+	}
+	$(".class_small").html(str_change);
+
+	//alert(eval(class_large[0].large_name)[0].small_name);
 	//for(i=0;i<class_data.selected_val)
-	str_change += "<option>"+class_data.selected_val+"</option>"; 
+	//str_change += "<option>"+class_data.selected_val+"</option>"; 
 });
 
 //alert(class_data.class_large_1[0].class_small_1);
--- js/test_data.js
+++ js/test_data.js
@@ -80,36 +80,33 @@ var	data = [
 ]
 
 
-var class_data = 
-	{
-		"class_large_1":[
-			{"class_small":"小类1_1"},
-			{"class_small":"小类1_2"},
-			{"class_small":"小类1_3"},	
-		],
-		"class_large_2":[
-				{"class_small":"小类2_1"},
-				{"class_small":"小类2_2"},
-				{"class_small":"小类2_3"},
-				{"class_small":"小类2_4"}
-		],
-		"class_large_3":[
-			{"class_small":"小类3_1"},
-			{"class_small":"小类3_2"},
-			{"class_small":"小类3_3"},
-			{"class_small":"小类3_4"},
-			{"class_small":"小类3_5"}
-		]
-	}
 
-var class_large = [
+
+var class_data = [
 	{"large_name":"class_large_1"},
 	{"large_name":"class_large_2"},
-	{"large_name":"class_large_3"},
+	{"large_name":"class_large_3"}
 ]
-var class_smalll = [
-	{"class_small_1":"小类1_1","class_small_2":"小类1_2","class_small_3":"小类1_3"},
-	{"class_small_1":"小类2_1","class_small_2":"小类2_2","class_small_3":"小类2_3","class_small_4":"小类2_4"}
+
+var class_large_1 = [
+	{"small_name":"小类1_1"},
+	{"small_name":"小类1_2"},
+	{"small_name":"小类1_3"}
+]
+
+var class_large_2 = [
+	{"small_name":"小类2_1"},
+	{"small_name":"小类2_2"},
+	{"small_name":"小类2_3"},
+	{"small_name":"小类2_4"}
+]
+
+var class_large_3 = [
+	{"small_name":"小类3_1"},
+	{"small_name":"小类3_2"},
+	{"small_name":"小类3_3"},
+	{"small_name":"小类3_4"},
+	{"small_name":"小类3_5"}
 ]
 
 		
